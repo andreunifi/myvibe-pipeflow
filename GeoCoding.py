a0e761b1315149a4d9cb44388da69d0de30b4414
andre <andrea.mugnaini3@stud.unifi.it>
2024-07-07 21:33:38 +0200

    updated loc

--- GeoCoding.py
+++ GeoCoding.py
@@ -7,14 +7,20 @@ def get_coordinates(location):
         'format': 'json',
         'addressdetails': 1
     }
-    response = requests.get(url, params=params)
-    data = response.json()
-    
-    if data:
-        lat = data[0].get('lat')
-        lon = data[0].get('lon')
-        return [float(lat), float(lon)]
-    else:
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
+        data = response.json()
+        
+        if data:
+            lat = data[0].get('lat')
+            lon = data[0].get('lon')
+            return [float(lat), float(lon)]
+        else:
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
         return None
-
-
+    except ValueError as e:
+        print(f"JSON decode error: {e}")
+        return None
